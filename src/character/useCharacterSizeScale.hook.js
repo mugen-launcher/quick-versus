be7c2c61832f7ddfe4b1c943df18c78f45916d17
neolao <contact@neolao.com>
2020-02-29 14:41:50 +0100

    Fix loading constants file

--- src/character/useCharacterSizeScale.hook.js
+++ src/character/useCharacterSizeScale.hook.js
@@ -10,13 +10,20 @@ export default function useCharacterSizeScale(character) {
   const environment = useEnvironment();
   const definition = useCharacterDefinition(character);
   if (!definition) {
-    return "Unknown";
+    return;
   }
 
   return useMemo(() => {
     const definitionPath = path.resolve(environment.currentDirectory, "chars", character.definition);
     const directoryPath = path.dirname(definitionPath);
-    const constantsFilename = ini.unsafe(definition.Files.cns);
+    let constantsFilename;
+    if (definition.Files.cns) {
+      constantsFilename = ini.unsafe(definition.Files.cns);
+    } else if (definition.Files.Cns) {
+      constantsFilename = ini.unsafe(definition.Files.Cns);
+    } else {
+      return;
+    }
     const constantsPath = path.resolve(directoryPath, constantsFilename);
 
     const constants = ini.parse(fs.readFileSync(constantsPath, "utf-8"));
