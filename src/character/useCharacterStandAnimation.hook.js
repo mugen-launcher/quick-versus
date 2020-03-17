f9bace71aefcd1196837e85a3f5c587dab9e63cb
neolao <contact@neolao.com>
2020-03-17 22:27:15 +0100

    Looking for HTML export folder from Fighter Factory to display stand animation

--- src/character/useCharacterStandAnimation.hook.js
+++ src/character/useCharacterStandAnimation.hook.js
@@ -23,13 +23,14 @@ export default function useCharacterStandAnimation(character, colorIndex = 1) {
   const definitionPath = path.resolve(environment.currentDirectory, "chars", character.definition);
   const directoryPath = path.dirname(definitionPath);
   const imagePathsByPriority = [
+    path.resolve(directoryPath, "images", `export_anim_${colorIndex}.gif`),
     path.resolve(directoryPath, `stand${colorIndex}.gif`),
     path.resolve(directoryPath, "stand.gif")
   ];
   if (character.hasOwnProperty(propertyName)) {
-    imagePathsByPriority.push(path.resolve(directoryPath, character[propertyName]));
-    imagePathsByPriority.push(path.resolve(environment.currentDirectory, character[propertyName]));
-    imagePathsByPriority.push(path.resolve(environment.currentDirectory, "chars", character[propertyName]));
+    imagePathsByPriority.unshift(path.resolve(directoryPath, character[propertyName]));
+    imagePathsByPriority.unshift(path.resolve(environment.currentDirectory, character[propertyName]));
+    imagePathsByPriority.unshift(path.resolve(environment.currentDirectory, "chars", character[propertyName]));
   }
 
   for (const imagePath of imagePathsByPriority) {
